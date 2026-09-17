b8c4a494f4e81396c835f6654382528ac111e32c
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2026-09-17 13:29:20 -0300

    added scripts
    added button script and added two methods and set it up in the inspector.

---
+++ Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+//notes you can add a image to button and drag it to Target Graphic in button editor and change it to sprite swap to have cool visuals
+public class ButtonBehavior : MonoBehaviour
+{
+    //Do button Stuff
+
+    public void SpawnButton()
+    {
+        //do spawn button stuff
+    }
+
+    public void KillButton()
+    {
+        //do kill button stuff
+    }
+}
--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -2,15 +2,6 @@ using UnityEngine;
 
 public class GameEvents : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //do game events here 
+    //subscribes? 
 }
