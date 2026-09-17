588674baa6dda92c8fc4f2325c3edddab319a829
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2026-09-17 13:49:36 -0300

    Debug Test
    Tested the button clicks to make sure they are working

--- Assets/Scripts/ButtonBehavior.cs
+++ Assets/Scripts/ButtonBehavior.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 //notes you can add a image to button and drag it to Target Graphic in button editor and change it to sprite swap to have cool visuals
+
 public class ButtonBehavior : MonoBehaviour
 {
     //Do button Stuff
@@ -8,10 +9,12 @@ public class ButtonBehavior : MonoBehaviour
     public void SpawnButton()
     {
         //do spawn button stuff
+        Debug.Log("Spawn Button Clicked");
     }
 
     public void KillButton()
     {
         //do kill button stuff
+        Debug.Log("Kill Button Clicked");
     }
 }
